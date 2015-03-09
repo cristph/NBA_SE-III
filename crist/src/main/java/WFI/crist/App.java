1ed05798522e4adf7091154b87e329ab262385c5
cristph <1174887809@qq.com>
2015-03-09 13:55:06 +0800

    test Project

---
+++ crist/src/main/java/WFI/crist/App.java
@@ -0,0 +1,13 @@
+package WFI.crist;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
