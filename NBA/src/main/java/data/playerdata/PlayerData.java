d6c0a01721a5164c74b44ba28e78e0681c64cdcb
Jack <Jack@hc_1994>
2015-03-14 16:56:19 +0800

    wu

--- NBA/src/main/java/data/playerdata/PlayerData.java
+++ NBA/src/main/java/data/playerdata/PlayerData.java
@@ -1,5 +1,6 @@
 package data.playerdata;
 
+import java.io.File;
 import java.util.ArrayList;
 
 import po.PlayerAllGamePO;
@@ -8,10 +9,30 @@ import dataservice.playerdataservice.PlayerDataService;
 
 
 public class PlayerData implements PlayerDataService {
+	
+	
 
 	public ArrayList<PlayerPO> getPlayerData() {
 		// TODO Auto-generated method stub
-		return null;
+		String path="/data/players/info";
+		
+		File rootFile=new File(path);
+		File array[]=rootFile.listFiles();
+		
+		PlayerDataReadService prService=new PlayerDataReader();
+		ArrayList<PlayerPO> list=new ArrayList<PlayerPO>();
+		for(int i=0;i<array.length;i++)
+		{
+			if(array[i].isFile())
+			{
+				String playerPath=array[i].getPath();
+				PlayerPO temp=prService.getPlayerPo(playerPath);
+				list.add(temp);
+			}
+		}
+		
+		
+		return list;
 	}
 
 	public ArrayList<PlayerAllGamePO> getPlayerGameData() {
--- NBA/src/main/java/data/playerdata/PlayerDataReader.java
+++ NBA/src/main/java/data/playerdata/PlayerDataReader.java
@@ -1,5 +1,9 @@
 package data.playerdata;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.HashMap;
 
 import po.PlayerGamePO;
@@ -7,8 +11,19 @@ import po.PlayerPO;
 
 public class PlayerDataReader implements PlayerDataReadService {
 
-	public PlayerPO getPlayerPo(String name) {
+	public PlayerPO getPlayerPo(String path) {
 		// TODO Auto-generated method stub
+		PlayerPO result=new PlayerPO();
+		
+		try {
+			FileReader inOne=new FileReader(new File(path));
+			BufferedReader inTwo=new BufferedReader(inOne);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 		return null;
 	}
 
