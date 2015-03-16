f0bdb8cccb6d294c27891abe3463e70d6947f50b
cristph <1174887809@qq.com>
2015-03-16 15:43:14 +0800

    add TeamInfo and TeamInfoProvider

---
+++ NBA/src/main/java/businesslogic/playerbl/ComparatorRate.java
@@ -0,0 +1,14 @@
+package businesslogic.playerbl;
+
+import java.util.Comparator;
+
+import vo.PlayerInfoVO;
+
+public class ComparatorRate implements Comparator<PlayerInfoVO>{
+
+	public int compare(PlayerInfoVO arg0, PlayerInfoVO arg1) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
