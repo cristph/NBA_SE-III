6d3d002b60ad4d0b19c7df745d895ef374432a83
nonetheless <ggg66166161@163.com>
2015-03-16 22:23:08 +0800

    更新了球员的vo

--- NBA/src/main/java/vo/PlayerVO.java
+++ NBA/src/main/java/vo/PlayerVO.java
@@ -1,5 +1,25 @@
 package vo;
 
+import java.awt.Image;
+
+/**
+ * 
+ * @author xinlin
+ *返回球员的所有信息
+ */
+
 public class PlayerVO {
+	String name;
+	String team;
+    String Position;//位置
+    String height;
+    double weight;
+    String birth;
+    int age;
+    int exp;
+    String school;
+    Image pic;
+    PlayerInfoVO  all;
+    PlayerInfoVO avg;//平均的
 
 }
