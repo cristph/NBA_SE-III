7064d66873e41e533ea128b4d08dcded286b0ae8
cristph <1174887809@qq.com>
2015-03-22 18:42:41 +0800

    update CalMethod

--- NBA/src/main/java/businesslogic/playerbl/CalMethod.java
+++ NBA/src/main/java/businesslogic/playerbl/CalMethod.java
@@ -3,10 +3,16 @@ package businesslogic.playerbl;
 public class CalMethod {
 	
 	public double calT(int time,int allPlayerTime){
+		if(time==0){
+			return 0;
+		}
 		return (1.0*(allPlayerTime/5)/time);
 	}
 	
 	public double calRate(int sample,int sum){
+		if(sum==0){
+			return 0;
+		}
 		return (1.0*sample/sum);
 	}
 	
@@ -25,35 +31,59 @@ public class CalMethod {
 	}
 	
 	public double calRealHitRate(int score, int shootNum, int freeNum){
+		if((shootNum+0.44*freeNum)==0){
+			return 0;
+		}
 		return 1.0*score/(2*(shootNum+0.44*freeNum));
 	}
 	
 	public double calThrowRate(int hitShootNum, int shootNum, int threePointNum){
+		if(shootNum==0){
+			return 0;
+		}
 		return  1.0*(hitShootNum+0.5*threePointNum)/shootNum;
 	}
 	
 	public double calRebRate(int rebTotalNum, double T, int teamRebNum, int oppTeamRebNum){
+		if((teamRebNum+oppTeamRebNum)==0){
+			return 0;
+		}
 		return 1.0*rebTotalNum*T/(teamRebNum+oppTeamRebNum);
 	}
 	
 	public double calAssistRate(int assistNum, int teamHitNum, int hitShootNum, double T){
+		if(T==0){
+			return 0;
+		}
 		return 1.0*assistNum/((1/T)*teamHitNum-hitShootNum);
 	}
 	
 	public double calStealRate(int stealNum, int oppAttNum, double T){
+		if(oppAttNum==0){
+			return 0;
+		}
 		return 1.0*stealNum*T/oppAttNum;
 	}
 	
 	public double calBlockRate(int blockNum, int oppTwoNum, double T){
+		if(oppTwoNum==0){
+			return 0;
+		}
 		return 1.0*blockNum*T/oppTwoNum;
 	}
 	
 	public double calErrorRate(int errorNum, int twoNum, int freeNum){
+		if((twoNum+0.44*freeNum+errorNum)==0){
+			return 0;
+		}
 		return 1.0*errorNum/(twoNum+0.44*freeNum+errorNum);
 	}
 	
 	public double calUseRate(int shootNum, int freeNum, int errorNum, double T, 
 			int teamThrowNum, int teamFreeNum, int teamErrorNum){
+		if((teamThrowNum+0.44*teamFreeNum+teamErrorNum)==0){
+			return 0;
+		}
 		return 1.0*(shootNum+0.44*freeNum+errorNum)*T/(teamThrowNum+0.44*teamFreeNum+teamErrorNum);
 	}
 }
