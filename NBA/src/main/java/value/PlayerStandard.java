a4d41ecdbbe48c77ac73454c5755d22f317f5f7b
cristph <1174887809@qq.com>
2015-04-22 22:42:04 +0800

    update playerStandard

--- NBA/src/main/java/value/PlayerStandard.java
+++ NBA/src/main/java/value/PlayerStandard.java
@@ -1,7 +1,7 @@
 package value;
 
 public enum PlayerStandard {
-	name,team,inTime,firstTime,rebTotalNum,assistNum,time,shooting,threeRate,freeRate,rebAttNum,rebDefNum,stealNum,
+	rebTotalNum,assistNum,time,shooting,threeRate,freeRate,stealNum,
 	blockNum,errorNum,foulNum,score,rate,GMSC,realHitRate,throwRate,rebRate,attRebRate,defRebRate,assistRate,
-	stealRate,blockRate,errorRate,usedRate,par,doub;
+	blockRate,errorRate,usedRate;
 }
